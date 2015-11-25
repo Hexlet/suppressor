c07f95933afc6187761e891b222fb6d31cf7abbc
Vagin Alexander <laplugin73@gmail.com>
2015-11-25 12:04:45 +0000

    prepare out

--- suppressor.py
+++ suppressor.py
@@ -15,20 +15,25 @@ import sys
 from subprocess import Popen, PIPE
 from docopt import docopt
 
-def main(docopt_args):
-    p = Popen(ARGS["COMMAND"], shell=True, stdout=PIPE, stderr=PIPE)
+def main():
+    docopt_args = docopt(__doc__)
+    p = Popen(docopt_args["COMMAND"], shell=True, stdout=PIPE, stderr=PIPE)
     output, err = p.communicate()
     if docopt_args["pass"]:
         if p.returncode != 0:
-            print(err.decode(), file=sys.stderr)
+            print(prepare_out(output), file=sys.stderr)
+            print(prepare_out(err), file=sys.stderr)
+            print("\033[91m Expected tests to pass, but error occurred. See output above. \033[0m", file=sys.stdout)
             exit(1)
 
     if docopt_args["fail"]:
         if p.returncode == 0:
-            print(output.decode(), file=sys.stderr)
+            print(prepare_out(output), file=sys.stderr)
+            print("\033[91m Expected tests to fail, but they passed. See output above. \033[0m", file=sys.stdout)
             exit(1)
 
-if __name__ == '__main__':
-    ARGS = docopt(__doc__)
-    main(ARGS)
+def prepare_out(output):
+    splitted_list = output.decode().split("\n")
+    joined_str = "\n#\t".join(item for item in splitted_list)
+    return "#\t" + joined_str
 
