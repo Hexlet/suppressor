3ae1db2ace5b71b39f2c5ebbaed7538c42ab6767
plugin73 <laplugin73@gmail.com>
2020-01-23 07:54:55 +0000

    improve prints

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # suppressor
+
+### Installation
+1. install go
+2. https://goreleaser.com/install/
+3. GITHUB_TOKEN= goreleaser
--- suppressor.go
+++ suppressor.go
@@ -9,6 +9,7 @@ import (
 
 func prepare_output(output string) string {
   splitted_list := strings.Split(output, "\n")
+
   joined_str := strings.Join(splitted_list, "\n#\t")
   return "#\t" + joined_str + "\n";
 }
@@ -17,24 +18,30 @@ func main() {
 
   mode := os.Args[1]
 
-  cmd := exec.Command("/bin/bash", "-c", os.Args[2])
+  args := strings.Split(os.Args[2], " ")
+  cmd := exec.Command(args[0], args[1:]...)
 
-  stdout, err := cmd.Output()
+  stdout, err := cmd.CombinedOutput()
 
-  fmt.Fprintf(os.Stdout, prepare_output(string(stdout)))
 
   if (mode == "pass") {
     if ( err != nil ) {
-      fmt.Fprintf(os.Stdout,"%s \n", prepare_output(err.Error()))
+      fmt.Fprintf(os.Stdout, prepare_output(string(stdout)))
+      fmt.Fprintf(os.Stdout,"%s \n", prepare_output(fmt.Sprint(err)))
       fmt.Fprintf(os.Stdout,"\033[91m Expected tests to pass, but error occurred. See output above. \033[0m \n")
       os.Exit(1)
+    } else {
+      fmt.Fprintf(os.Stdout,"\u001b[32m Expected tests to pass, recieved tests passed\033[0m \n")
     }
   }
 
   if (mode == "fail") {
     if ( err == nil ) {
+      fmt.Fprintf(os.Stdout, prepare_output(string(stdout)))
       fmt.Fprintf(os.Stdout, "\033[91m Expected tests to fail, but they passed. See output above. \033[0m \n")
       os.Exit(1)
+    } else {
+      fmt.Fprintf(os.Stdout,"\u001b[32m Expected tests to fail, recieved tests failed\033[0m \n")
     }
   }
 }
